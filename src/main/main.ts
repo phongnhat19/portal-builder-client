44fa6ffe26e05fba358ba20a27dad605a4934f12
Nhat Nguyen <phongnhat19@gmail.com>
2020-03-16 15:51:22 +0700

    define custom event for main process

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import {requestToKintone} from './util/kintone'
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -15,6 +16,9 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     height: 768,
     width: 1366,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html of the app.
@@ -56,3 +60,11 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+// Event listener for kintone
+ipcMain.on('request-to-kintone', (event: Electron.IpcMainEvent, arg: any) => {
+  console.log(
+    arg
+  );
+  requestToKintone()
+})
