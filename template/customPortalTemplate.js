f50eaa216c0e18cb1ed47ca3ab5fe0b4a16cc38a
Ho Kim Cuc <c001106@PC-001106-4.local>
2020-03-17 13:00:09 +0700

    add template file

---
+++ template/customPortalTemplate.js
@@ -0,0 +1,73 @@
+  const configs = PORTAL_CONFIG
+
+  function createDefaultPortalContent() {
+    const portalSpaceEl = kintone.portal.getContentSpaceElement();
+    const defaultPortalBodyEl = portalSpaceEl.nextSibling;
+
+    const containerEl = document.createElement('div');
+    containerEl.appendChild(defaultPortalBodyEl);
+
+    return containerEl;
+  }
+
+  function createIframeItemContent(props) {
+    const iframeEl = document.createElement('iframe');
+    iframeEl.src = props.url;
+    iframeEl.style.width = props.width;
+    iframeEl.style.height = props.height;
+
+    return iframeEl;
+  }
+
+  function createHTMLItemContent(props) {
+    const htmlEl = document.createElement('div');
+    htmlEl.innerHTML = props.src;
+
+    return htmlEl;
+  }
+
+  function createTabContent(content) {
+    let tabContent;
+    switch (content.type) {
+      case 'DefaultPortal':
+        tabContent = createDefaultPortalContent();
+        break;
+      case 'IframeWidget':
+        tabContent = createIframeItemContent(content.props);
+        break;
+      case 'HTMLWidget':
+        tabContent = createHTMLItemContent(content.props);
+        break;
+
+      default:
+        tabContent = '';
+    }
+
+    return tabContent;
+  }
+
+  function createTabItem(name, content) {
+    return {
+      tabName: name,
+      tabContent: createTabContent(content)
+    };
+  };
+
+  function createTabs() {
+    const tabItems = [];
+
+    configs.forEach(function (item) {
+      tabItems.push(createTabItem(item.tabName, item.tabContent));
+    });
+
+    return new kintoneUIComponent.Tabs({ items: tabItems });
+  }
+
+  kintone.events.on('portal.show', function (event) {
+    const portalSpaceEl = kintone.portal.getContentSpaceElement();
+
+    const tabs = createTabs();
+    portalSpaceEl.appendChild(tabs.render());
+
+    return event;
+  });
