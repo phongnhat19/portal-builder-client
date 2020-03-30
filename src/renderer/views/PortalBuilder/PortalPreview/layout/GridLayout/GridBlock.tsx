e0c9dccb199f1604bf6005a894cff74da8f7521e
minhhy2801 <minhhy2897@gmail.com>
2020-03-30 15:03:57 +0700

    template grid block drag drop

--- src/renderer/views/PortalBuilder/PortalPreview/layout/GridLayout/GridBlock.tsx
+++ src/renderer/views/PortalBuilder/PortalPreview/layout/GridLayout/GridBlock.tsx
@@ -1,11 +1,19 @@
-import React, { CSSProperties, ReactElement, useRef } from 'react'
+import React, { CSSProperties, useRef, useContext, useState, useEffect } from 'react'
 import './style.css'
-import { MinusCircleOutlined } from '@ant-design/icons';
+import { MinusCircleOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import { Button, Popconfirm, Row } from 'antd';
+import { PortalContext } from '../../..';
+import { SCHEDULE_VIEW } from '../../../Widget/ScheduleWidget/constant';
+import IframeWidget from '../../../Widget/IframeWidget';
+import HTMLWidget from '../../../Widget/HTMLWidget';
+import ScheduleWidget from '../../../Widget/ScheduleWidget';
+import { EMPTY_WIDGET_CONTENT, CONFIRM_DELETE } from '../TabsLayout/constant';
+import confirm from 'antd/lib/modal/confirm';
+import { CONTENT_TYPE } from '../../../Widget/constant';
 
-const GridBlock = ({ style, content, width, rowIndex, blockIndex, onRemoveBlock, onResizeWidth }: {
+const GridBlock = ({ style, content = undefined, width, rowIndex, blockIndex, onRemoveBlock, onResizeWidth }: {
   style?: CSSProperties
-  content: ReactElement | string | number
+  content?: IframeWidgetProps | HTMLWidgetProps | ScheduleWidgetProps
   width: number
   rowIndex: number
   blockIndex: number
@@ -21,12 +29,154 @@ const GridBlock = ({ style, content, width, rowIndex, blockIndex, onRemoveBlock,
     finalStyle = {...finalStyle, ...style}
   }
 
+  const { portalList, setPortalList, selectedPortal } = useContext(PortalContext)
+  const [blockContent, setBlockContent] = useState(null)
+
   const blockRef = useRef<HTMLDivElement>(null)
-  
-  return(
-    <div ref={blockRef} style={finalStyle} className="grid-block" onMouseUp={() => {
-      onResizeWidth({width: blockRef.current!.offsetWidth})
-    }}>
+
+  const dropWidget = (rowIndex: number, blockIndex: number, type: ContentType, props: any) => {
+    const gridLayout = portalList[selectedPortal].layout.props as GridLayout
+    const currentBlock = gridLayout.rows[rowIndex].blocks[blockIndex]
+    if (currentBlock.type === CONTENT_TYPE.EMPTY) {
+      currentBlock.type = type
+      currentBlock.content = props
+      setPortalList(portalList)
+    }
+  }
+
+  const updateWidget = (newProps: IframeWidgetProps | HTMLWidgetProps | ScheduleWidgetProps) => {
+    const gridLayout = portalList[selectedPortal].layout.props as GridLayout
+    const currentBlock = gridLayout.rows[rowIndex].blocks[blockIndex]
+    currentBlock.content = newProps
+    setPortalList(portalList)
+  }
+
+  const removeWidget = () => {
+    confirm({
+      title: CONFIRM_DELETE.TITLE,
+      icon: <ExclamationCircleOutlined />,
+      okText: CONFIRM_DELETE.BUTTON_OK,
+      okType: 'danger',
+      cancelText: CONFIRM_DELETE.BUTTON_CANCEL,
+      onOk() {
+        const gridLayout = portalList[selectedPortal].layout.props as GridLayout
+        const currentBlock = gridLayout.rows[rowIndex].blocks[blockIndex]
+        currentBlock.type = CONTENT_TYPE.EMPTY as ContentType
+        delete currentBlock.content
+        setPortalList(portalList)
+      }
+    })
+  }
+
+  const buildContent = () => {
+    const gridLayout = portalList[selectedPortal].layout.props as GridLayout
+    const currentBlock = gridLayout.rows[rowIndex].blocks[blockIndex]
+    let currentContentBlock = EMPTY_WIDGET_CONTENT as any
+
+    if (!currentBlock) return;
+    switch (currentBlock.type) {
+      case CONTENT_TYPE.IFRAME:
+        if (!currentBlock.content) {
+          break;
+        }
+        const blockContentIframe = currentBlock.content as IframeWidgetProps;
+
+        currentContentBlock =
+          <IframeWidget
+            url={blockContentIframe.url}
+            // width={blockContentIframe.width}
+            // height={blockContentIframe.height}
+            showSettingInit={blockContentIframe.showSettingInit}
+            onRemove={removeWidget}
+            onSaveSetting={({ url }) => {
+              let currentProps = JSON.parse(JSON.stringify(currentBlock.content))
+              currentProps.url = url
+              currentProps.showSettingInit = false;
+              updateWidget(currentProps)
+            }}
+          />
+        break;
+
+      case CONTENT_TYPE.HTML:
+        if (!currentBlock.content) {
+          break;
+        };
+        const blockContentHTML = currentBlock.content as HTMLWidgetProps;
+        currentContentBlock =
+          <HTMLWidget
+            htmlString={blockContentHTML.htmlString}
+            // width={blockContentHTML.width}
+            // height={blockContentHTML.height}
+            showSettingInit={blockContentHTML.showSettingInit}
+            onRemove={removeWidget}
+            onSaveSetting={({ htmlString }) => {
+              let currentProps = JSON.parse(JSON.stringify(currentBlock.content))
+              currentProps.htmlString = htmlString
+              currentProps.showSettingInit = false;
+              updateWidget(currentProps)
+            }}
+          />
+        break;
+      case CONTENT_TYPE.SCHEDULE:
+        if (!currentBlock.content) {
+          break;
+        };
+        const blockContentSchedule = currentBlock.content as ScheduleWidgetProps;
+        currentContentBlock =
+          <ScheduleWidget
+            defaultView={blockContentSchedule.defaultView}
+            onRemove={removeWidget}
+            onSaveSetting={({ defaultView }) => {
+              let currentProps = JSON.parse(JSON.stringify(currentBlock.content))
+              currentProps.defaultView = defaultView
+              currentProps.showSettingInit = false;
+              updateWidget(currentProps)
+            }}
+          />
+        break;
+      case CONTENT_TYPE.EMPTY:
+        currentContentBlock = EMPTY_WIDGET_CONTENT
+      default:
+        break;
+    }
+    return currentContentBlock
+  }
+
+  useEffect(() => {
+    setBlockContent(buildContent())
+  }, [content])
+
+  return (
+    <div
+      ref={blockRef}
+      style={finalStyle}
+      className="grid-block"
+      onMouseUp={() => {
+        onResizeWidth({ width: blockRef.current!.offsetWidth })
+      }}
+      onDragOver={(event: React.DragEvent<HTMLDivElement>) => { event.preventDefault(); }}
+      onDrop={(e) => {
+        let props: any
+        const type = e.dataTransfer.getData("text") as ContentType
+        if (type === CONTENT_TYPE.IFRAME) {
+          props = {
+            showSettingInit: true,
+            url: "",
+          }
+        } else if (type === CONTENT_TYPE.HTML) {
+          props = {
+            showSettingInit: true,
+            htmlString: "",
+          }
+        } else if (type === CONTENT_TYPE.SCHEDULE) {
+          props = {
+            showSettingInit: true,
+            defaultView: SCHEDULE_VIEW.FULL_CALENDAR_DAY_TIME
+          }
+        }
+        props && dropWidget(rowIndex, blockIndex, type, props)
+      }}
+    >
       <Row>
         <Popconfirm
           title="Are you sure to delete this block?"
@@ -43,7 +193,7 @@ const GridBlock = ({ style, content, width, rowIndex, blockIndex, onRemoveBlock,
         </Popconfirm>
       </Row>
       <Row>
-        {content}
+        {blockContent}
       </Row>
     </div>
   )
