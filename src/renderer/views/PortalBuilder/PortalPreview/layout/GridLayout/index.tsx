f18f451d39f3da0bc5d3d791cc15e5a9a7e8f77a
minhhy2801 <minhhy2897@gmail.com>
2020-04-01 15:46:03 +0700

    change color

--- src/renderer/views/PortalBuilder/PortalPreview/layout/GridLayout/index.tsx
+++ src/renderer/views/PortalBuilder/PortalPreview/layout/GridLayout/index.tsx
@@ -54,7 +54,7 @@ const GridLayout = ({ items = [] }: {
             type="dashed"
             icon={<PlusCircleOutlined />}
             className="grid-layout-btn-add"
-            style={{ width: '100%', minHeight: '100px', backgroundColor: '#facfa7', borderColor: '#e3a871' }}
+            style={{ width: '100%', minHeight: '100px', backgroundColor: '#fef2e7', borderColor: '#fce0c7' }}
             onClick={() => {
               const sampleRow = {
                 blocks: [{
