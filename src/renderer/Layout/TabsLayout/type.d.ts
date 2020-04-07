3f9674d796e005bc26cc99e5332651f5e2934dbd
Nhat Nguyen <phongnhat19@gmail.com>
2020-04-07 15:10:37 +0700

    move Tab type into layout

--- src/renderer/Layout/TabsLayout/type.d.ts
+++ src/renderer/Layout/TabsLayout/type.d.ts
@@ -1,3 +1,13 @@
+declare type Tab = {
+  tabName: string,
+  tabContent: {
+    type: ContentType,
+    name?: string,
+    props?: IframeWidgetProps | HTMLWidgetProps | SchedulerWidgetProps,
+    value?: string
+  }
+}
+
 declare type TabLayout = {
   tabList: Tab[]
 }
--- src/renderer/views/PortalBuilder/PortalPreview/type.d.ts
+++ src/renderer/views/PortalBuilder/PortalPreview/type.d.ts
@@ -5,13 +5,3 @@ declare enum ContentType {
   DEFAULT = 'DefaultPortal',
   EMPTY = 'Empty'
 }
-
-declare type Tab = {
-  tabName: string,
-  tabContent: {
-    type: ContentType,
-    name?: string,
-    props?: IframeWidgetProps | HTMLWidgetProps | SchedulerWidgetProps,
-    value?: string
-  }
-}
