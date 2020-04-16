4047b57a897e5afb68e68883cce2da6a301f977f
Cuc Ho <cuc-ho@cybozu.vn>
2020-04-16 16:59:18 +0700

    fix change view in config

--- src/renderer/Widget/SchedulerWidget/renderer.tsx
+++ src/renderer/Widget/SchedulerWidget/renderer.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -13,16 +13,22 @@ const Scheduler = ({defaultView, data = []}: {
   defaultView?: string;
   data?: SchedulerEvent[];
 }) => {
-
+  const calendarRef = useRef();
   const [events, setEvents] = useState(data);
 
   useEffect(() => {
     if (data.length === 0 && window.kintone) getSchedulerEvent().then(setEvents);
   }, [data.length]);
 
+  useEffect(() => {
+    const calendarApi = (calendarRef.current)!.getApi(0);
+    calendarApi.changeView(defaultView);
+  }, [defaultView]);
+
   return (
     <div style={{backgroundColor: '#FFFFFF'}}>
       <FullCalendar
+        ref={calendarRef}
         header={{
           left: `prev,next ${SCHEDULER_VIEW.FULL_CALENDAR_DAY_TIME}, ${SCHEDULER_VIEW.FULL_CALENDAR_WEEK_TIME}`,
           center: 'title',
