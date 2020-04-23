8cdb06ba68ea3cd88a7540526851dcde20813d35
huong <phamgiahuong2012@gmail.com>
2020-04-23 12:12:16 +0700

    implement ui view

--- src/renderer/Widget/AppSpaceListWidget/rerender.tsx
+++ src/renderer/Widget/AppSpaceListWidget/rerender.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
-const AppSpace = ({listContent}:{listContent:any}) => {
-  return <div className="app-space-widget">
-    <div className="">preview</div>
-  </div>;
+import React, {useState, useEffect} from 'react';
+import {getAppInfo} from './service';
+import './style.css';
+import 'antd/dist/antd.css';
+import PreviewTitle from './components/PreviewTitle';
+import PreviewContent from './components/PreviewContent';
+import Note from './components/Note';
+
+const AppSpace = ({listContent = [], titleWidget = ''}: {listContent: ModalAppSpace[]; titleWidget: string}) => {  
+  const [listAppSpace, setListAppSpace] = useState(listContent);
+  const [titleAppSpace, setTitleAppSpace] = useState(titleWidget); 
+  
+  useEffect(() => {        
+    (async () => {
+      if (window.kintone) {
+        let newListContent = await getAppInfo({listAppSpace});
+        setTitleAppSpace(titleWidget);
+        setListAppSpace(newListContent);
+      } else {
+        console.log("else listContent",listContent);
+        
+        setTitleAppSpace(titleWidget);
+        setListAppSpace(listContent);
+      }
+    })();
+  });
+
+  if (listAppSpace.length === 0) {
+    return null;
+  }
+  console.log("listContent",listContent);
+  console.log("listAppSpace",listAppSpace);
+  
+  return (
+    <div className="app-space-widget">
+      <PreviewTitle titleWidget={titleAppSpace} />
+      {window.kintone ? "" :  <Note />}
+      <PreviewContent listContent={listAppSpace} />
+    </div>
+  );
 };
 export default AppSpace;
--- src/renderer/Widget/AppSpaceListWidget/type.d.ts
+++ src/renderer/Widget/AppSpaceListWidget/type.d.ts
@@ -1,15 +1,18 @@
 declare type AppSpaceWidgetProps = {
   showSettingInit?: boolean;
-  listContent:any;
-  titleWidget:string
+  listContent: ModalAppSpace[];
+  titleWidget: string;
+  isSave:boolean
 };
 
 declare type CategorytAppSpace = {
   type: string;
   id: string;
+  icon: string;
+  name:string;
 };
 
 declare type ModalAppSpace = {
   category: string;
-  listCategory:ContentAppSpaceWidget[]
+  listCategory: ContentAppSpaceWidget[];
 };
