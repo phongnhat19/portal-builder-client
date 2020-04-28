83a0373d0288eebe5117bf3aa13d090eee3ac909
huong <phamgiahuong2012@gmail.com>
2020-04-28 10:19:55 +0700

    add class for setting model

--- src/renderer/Widget/components/Settings.tsx
+++ src/renderer/Widget/components/Settings.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {Dropdown, Button, Menu} from 'antd';
 import {SettingOutlined, DeleteOutlined, FormOutlined} from '@ant-design/icons';
 
-const SettingsWidget = ({onRemove, showSetting}: {
-  onRemove?: () => void;
-  showSetting?: () => void;
-}) => {
-
+const SettingsWidget = ({onRemove, showSetting, className}: {onRemove?: () => void; 
+  showSetting?: () => void; className?: string}) => {
+  let _className = '';
+  if (className) {
+    _className = className;
+  }
   const menu = (
     <Menu>
       <Menu.Item key="remove-widget" onClick={onRemove}>
@@ -21,7 +22,7 @@ const SettingsWidget = ({onRemove, showSetting}: {
   );
 
   return (
-    <div className="portal-tabs-btn portal-tabs-btn-settings">
+    <div className={`portal-tabs-btn portal-tabs-btn-settings ${_className}`}>
       <Dropdown overlay={menu}>
         <Button>
           <SettingOutlined />
