4e1d7b0ce6cab3ecce869cc616827f19d4ab0e9f
Nhat Nguyen <phongnhat19@gmail.com>
2020-05-15 11:23:43 +0700

    lint Weather widget

--- src/renderer/Widget/WeatherWidget/FullWeather.tsx
+++ src/renderer/Widget/WeatherWidget/FullWeather.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Row, Col } from 'antd';
-import Weather from 'simple-react-weather'
+import React from 'react';
+import {Row, Col} from 'antd';
+import Weather from 'simple-react-weather';
 
-const FullWeather = ({ unitTemp, weatherCity, openWeatherMapAPIKey, weatherData }: {
-  unitTemp: string, weatherCity: string, openWeatherMapAPIKey: string, weatherData: FullWeatherProps
+const FullWeather = ({unitTemp, weatherCity, openWeatherMapAPIKey, weatherData}: {
+  unitTemp: string; weatherCity: string; openWeatherMapAPIKey: string; weatherData: FullWeatherProps;
 }) => {
 
   return (
@@ -11,14 +11,14 @@ const FullWeather = ({ unitTemp, weatherCity, openWeatherMapAPIKey, weatherData 
       <Col span={8}>
         <Weather unit={unitTemp} city={weatherCity} appid={openWeatherMapAPIKey} />
       </Col>
-      <Col span={16} className='widget-full-weather'>
+      <Col span={16} className="widget-full-weather">
         <p><strong>weather:</strong> {weatherData.description}</p>
         <p><strong>humidity:</strong> {weatherData.humidity}</p>
         <p><strong>wind speed:</strong> {weatherData.windSpeed}</p>
         <p><strong>cloud:</strong> {weatherData.cloud}</p>
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default FullWeather
+export default FullWeather;
--- src/renderer/Widget/WeatherWidget/WeatherModal.tsx
+++ src/renderer/Widget/WeatherWidget/WeatherModal.tsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
-import { Modal, Row, Col, Radio, Input, Alert } from 'antd'
-import { WEATHER_UNIT, WEATHER_TYPE } from './constant';
+import React, {useState} from 'react';
+import {Modal, Row, Col, Radio, Input, Alert} from 'antd';
+import {WEATHER_UNIT, WEATHER_TYPE} from './constant';
 
-declare type WeatherModal = { city: string, unit: string, apiKey: string, type: string }
-const WeatherModal = ({ isVisible = false, onClose, onSave, defaultCity, defaultUnit, defaultAPIKey, defaultType, error }: {
-  defaultCity: string
-  defaultUnit: string
-  defaultAPIKey: string
-  defaultType: string
-  isVisible: boolean
+declare type WeatherModal = { city: string; unit: string; apiKey: string; type: string }
+const WeatherModal = ({isVisible = false, onClose, onSave, defaultCity, defaultUnit, defaultAPIKey, defaultType, error}: {
+  defaultCity: string;
+  defaultUnit: string;
+  defaultAPIKey: string;
+  defaultType: string;
+  isVisible: boolean;
   error: {
     city?: string;
     api?: string;
   };
-  onSave: (item: WeatherModal) => void
-  onClose?: () => void
+  onSave: (item: WeatherModal) => void;
+  onClose?: () => void;
 }) => {
 
-  const [city, setCity] = useState(defaultCity)
-  const [weatherUnit, setWeatherUnit] = useState(defaultUnit)
-  const [apiKey, setApiKey] = useState(defaultAPIKey)
-  const [type, setType] = useState(defaultType)
+  const [city, setCity] = useState(defaultCity);
+  const [weatherUnit, setWeatherUnit] = useState(defaultUnit);
+  const [apiKey, setApiKey] = useState(defaultAPIKey);
+  const [type, setType] = useState(defaultType);
 
   return (
     <Modal
@@ -40,7 +40,7 @@ const WeatherModal = ({ isVisible = false, onClose, onSave, defaultCity, default
           apiKey,
           unit: weatherUnit,
           type,
-        })
+        });
       }}
     >
       <Row>
@@ -48,51 +48,65 @@ const WeatherModal = ({ isVisible = false, onClose, onSave, defaultCity, default
           <strong>Type show</strong>
         </Col>
         <Col span={20}>
-          <Radio.Group onChange={(e) => { setType(e.target.value) }} value={type}>
+          <Radio.Group
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+            value={type}
+          >
             <Radio.Button value={WEATHER_TYPE.SIMPLE}>{WEATHER_TYPE.SIMPLE}</Radio.Button>
             <Radio.Button value={WEATHER_TYPE.FULL_INFO}>{WEATHER_TYPE.FULL_INFO}</Radio.Button>
           </Radio.Group>
         </Col>
       </Row>
-      <Row className='widget-config-weather'>
+      <Row className="widget-config-weather">
         <Col span={4}>
           <strong>Unit</strong>
         </Col>
         <Col span={20}>
-          <Radio.Group onChange={(e) => { setWeatherUnit(e.target.value) }} value={weatherUnit}>
+          <Radio.Group
+            onChange={(e) => {
+              setWeatherUnit(e.target.value);
+            }}
+            value={weatherUnit}
+          >
             <Radio.Button value={WEATHER_UNIT.CELCIUS}>{WEATHER_UNIT.CELCIUS_TITLE}</Radio.Button>
             <Radio.Button value={WEATHER_UNIT.FAHRENHEIT}>{WEATHER_UNIT.FAHRENHEIT_TITLE}</Radio.Button>
           </Radio.Group>
         </Col>
       </Row>
-      <Row className='widget-config-weather'>
+      <Row className="widget-config-weather">
         <Col span={4}>
           <strong>City</strong>
         </Col>
         <Col span={20}>
           <Input
             value={city}
-            onChange={(e) => { setCity(e.target.value) }}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
             placeholder="Input City"
           />
           {error.city && <Alert message={error.city} type="error" /> }
         </Col>
       </Row>
-      <Row className='widget-config-weather'>
+      <Row className="widget-config-weather">
         <Col span={4}>
           <strong>API Key</strong>
         </Col>
         <Col span={20}>
           <Input
             value={apiKey}
-            onChange={(e) => { setApiKey(e.target.value) }}
+            onChange={(e) => {
+              setApiKey(e.target.value);
+            }}
             placeholder="Input OpenWeatherMap API Key"
           />
           {error.api && <Alert message={error.api} type="error" /> }
         </Col>
       </Row>
     </Modal>
-  )
-}
+  );
+};
 
-export default WeatherModal
+export default WeatherModal;
