601e5a379fb047d3d6113e8d7c850a8d12cbb76b
Nhat Nguyen <phongnhat19@gmail.com>
2020-05-15 11:23:51 +0700

    lint Scheduler widget

--- src/renderer/Widget/SchedulerWidget/ScheduleModal.tsx
+++ src/renderer/Widget/SchedulerWidget/ScheduleModal.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import { Modal, Row, Col, Radio } from 'antd'
-import { SCHEDULER_VIEW } from './constant';
+import React, {useState} from 'react';
+import {Modal, Row, Col, Radio} from 'antd';
+import {SCHEDULER_VIEW} from './constant';
 
-const SchedulerModal = ({ isVisible = false, onClose, onSave, defaultView }: {
-  defaultView: string
-  isVisible: boolean
-  onSave: (item: { defaultView: string }) => void
-  onClose?: () => void
+const SchedulerModal = ({isVisible = false, onClose, onSave, defaultView}: {
+  defaultView: string;
+  isVisible: boolean;
+  onSave: (item: { defaultView: string }) => void;
+  onClose?: () => void;
 }) => {
 
-  const [typeView, setTypeView] = useState(defaultView)
+  const [typeView, setTypeView] = useState(defaultView);
 
   return (
     <Modal
@@ -23,7 +23,7 @@ const SchedulerModal = ({ isVisible = false, onClose, onSave, defaultView }: {
       onOk={() => {
         onSave({
           defaultView: typeView
-        })
+        });
       }}
     >
       <Row>
@@ -31,14 +31,19 @@ const SchedulerModal = ({ isVisible = false, onClose, onSave, defaultView }: {
           <strong>View of calendar</strong>
         </Col>
         <Col span={20}>
-          <Radio.Group onChange={(e) => {setTypeView(e.target.value)}} value={typeView}>
+          <Radio.Group
+            onChange={(e) => {
+              setTypeView(e.target.value);
+            }}
+            value={typeView}
+          >
             <Radio.Button value={SCHEDULER_VIEW.FULL_CALENDAR_DAY_TIME}>{SCHEDULER_VIEW.DAY_VIEW}</Radio.Button>
             <Radio.Button value={SCHEDULER_VIEW.FULL_CALENDAR_WEEK_TIME}>{SCHEDULER_VIEW.WEEK_VIEW}</Radio.Button>
           </Radio.Group>
         </Col>
       </Row>
     </Modal>
-  )
-}
+  );
+};
 
-export default SchedulerModal
+export default SchedulerModal;
