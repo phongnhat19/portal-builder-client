9f00b3876651c244cc794f06ec8987fd5fa75453
Nhat Nguyen <phongnhat19@gmail.com>
2020-05-15 11:24:45 +0700

    lint portal builder's header

--- src/renderer/views/components/Header/SettingModal.tsx
+++ src/renderer/views/components/Header/SettingModal.tsx
@@ -1,25 +1,25 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { Modal, Form, Select, Input } from 'antd'
-import {ProfileContext} from '../../../App'
+import React, {useContext, useState, useEffect} from 'react';
+import {Modal, Form, Select, Input} from 'antd';
+import {ProfileContext} from '../../../App';
 
 const formItemLayout = {
-  labelCol: { span: 6 },
-  wrapperCol: { span: 18 },
+  labelCol: {span: 6},
+  wrapperCol: {span: 18},
 };
 
-const { Option } = Select
+const {Option} = Select;
 
 const defaultValue = {
   name: '',
   domain: '',
   username: '',
   password: ''
-}
+};
 
-const SettingModal = ({ isVisible = false, onClose }: {
-  isVisible?: boolean
-  onClose?: () => void
-  saveSetting?: (settingData: any) => void
+const SettingModal = ({isVisible = false, onClose}: {
+  isVisible?: boolean;
+  onClose?: () => void;
+  saveSetting?: (settingData: any) => void;
 }) => {
   const [form] = Form.useForm();
   const {profiles, setProfiles} = useContext(ProfileContext);
@@ -27,7 +27,7 @@ const SettingModal = ({ isVisible = false, onClose }: {
 
   useEffect(() => {
     setRequiredField(isVisible);
-    form.validateFields();
+    // form.validateFields();
   }, [isVisible]);
 
   return (
@@ -52,15 +52,15 @@ const SettingModal = ({ isVisible = false, onClose }: {
                 return values as Profile;
               }
               return profile;
-            })
+            });
 
             if (!isEditProfile) {
-              values.profileId = values.name + Math.random()
-              newProfiles = [...profiles, values as Profile]
+              values.profileId = values.name + Math.random();
+              newProfiles = [...profiles, values as Profile];
             }
-            setProfiles(newProfiles)
-            window.localStorage.setItem("profiles", JSON.stringify(newProfiles))
-            onClose && onClose()
+            setProfiles(newProfiles);
+            window.localStorage.setItem('profiles', JSON.stringify(newProfiles));
+            onClose && onClose();
           })
           .catch(info => {
             console.log('Validate Failed:', info);
@@ -81,48 +81,49 @@ const SettingModal = ({ isVisible = false, onClose }: {
           <Select
             placeholder="Please select a Profile"
             onChange={(value) => {
-              const result = profiles.filter(obj => obj.profileId === value)
-              form.setFieldsValue(result[0] || defaultValue)
-            }}>
-              {
-                profiles.map((profile, i) => {
-                  return <Option key={i} value={profile.profileId}>{profile.name}</Option>
-                })
-              }
+              const result = profiles.filter(obj => obj.profileId === value);
+              form.setFieldsValue(result[0] || defaultValue);
+            }}
+          >
+            {
+              profiles.map((profile, i) => {
+                return <Option key={i} value={profile.profileId}>{profile.name}</Option>;
+              })
+            }
             <Option value="profile_add">Add new profile...</Option>
           </Select>
         </Form.Item>
         <Form.Item
           name="name"
           label="Name"
-          rules={[{ required: requiredField, message: 'Please input your profile name!' }]}
+          rules={[{required: requiredField, message: 'Please input your profile name!'}]}
         >
           <Input />
         </Form.Item>
         <Form.Item
           name="domain"
           label="Domain"
-          rules={[{ required: requiredField, message: 'Please input your kintone domain!' }]}
+          rules={[{required: requiredField, message: 'Please input your kintone domain!'}]}
         >
           <Input />
         </Form.Item>
         <Form.Item
           name="username"
           label="Username"
-          rules={[{ required: requiredField, message: 'Please input your kintone username!' }]}
+          rules={[{required: requiredField, message: 'Please input your kintone username!'}]}
         >
           <Input />
         </Form.Item>
         <Form.Item
           name="password"
           label="Password"
-          rules={[{ required: requiredField, message: 'Please input your kintone password!' }]}
+          rules={[{required: requiredField, message: 'Please input your kintone password!'}]}
         >
           <Input.Password />
         </Form.Item>
       </Form>
     </Modal>
-  )
-}
+  );
+};
 
-export default SettingModal
+export default SettingModal;
